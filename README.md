4654559a97edfc943eebee69ddebed1ce1101e4d
George Constantinides <George007005001002@gmail.com>
2022-08-09 15:51:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# es-module-reproduction
+ 
---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <vueQr :text="code2FA"></vueQr>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+import vueQr from 'vue-qr/src/packages/vue-qr.vue';
+</script>
